5257a15a8cecf1e622c9568a646b19510439403d
João Abner <joaoabner2022@gmail.com>
2024-10-14 11:46:09 -0300

    Add function getHumanChoice

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 
 
 
-function getComputerChoice (){
+function getComputerChoice(){
     let number = Math.random() * 10;
     let choice;
 
     if (number < 4) {
-        choice = 'Papel';
+        choice = 'papel';
     } else if (number < 7) {
-        choice = 'Pedra';
+        choice = 'pedra';
     } else {
-        choice = 'Tesoura';
+        choice = 'tesoura';
     }
 
     return choice;
 }
+
+function getHumanChoice(){
+    let choice = prompt('Pedra, Papel ou Tesoura?').toLowerCase();
+    
+    if (choice == 'pedra' || choice == 'papel' || choice == 'tesoura') {
+        return choice;
+    } else { return 'opção inválida' }
+
+}
+
